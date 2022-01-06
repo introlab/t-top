f4a783922b5e3c038761215b5800511aa7ff3521
Marc-Antoine Maheux <Marc-Antoine.Maheux@USherbrooke.ca>
2022-01-06 17:44:33 -0500

    Add an id to the talk node messages.

--- ros/behaviors/talk/README.md
+++ ros/behaviors/talk/README.md
@@ -8,15 +8,16 @@ This node makes T-Top talk and move its lips accordingly. It uses Google Cloud T
 #### Parameters
  - `language` (string): The language (en or fr).
  - `mouth_signal_gain` (double): The gain to apply after the filter calculating the mouth signal.
+ - `sampling_frequency` (int): The output sampling frequency.
  - `frame_sample_count` (double): The number of sample in each audio frame.
 
 #### Subscribed Topics
-- `talk/text` ([std_msgs/String](http://docs.ros.org/en/noetic/api/std_msgs/html/msg/String.html)): The text to be said.
+- `talk/text` ([talk/Text](msg/Text.msg)): The text to be said.
 
 #### Published Topics
  - `face/mouth_signal_scale` ([std_msgs/Float32](http://docs.ros.org/en/noetic/api/std_msgs/html/msg/Float32.html)): Indicates how much the mouth must be open.
  - `audio_out` ([audio_utils/AudioFrame](https://github.com/introlab/audio_utils/blob/main/msg/AudioFrame.msg)): The speech sound.
- - `talk/done` ([std_msgs/Empty](http://docs.ros.org/en/noetic/api/std_msgs/html/msg/Empty.html)): Indicates that the speech is finished.
+ - `talk/done` ([talk/Done](msg/Done.msg)): Indicates that the speech is finished.
 
 #### Services
  - `audio_out/filter_state` ([hbba_lite/SetOnOffFilterState](../../hbba_lite/srv/SetOnOffFilterState.srv)): The HBBA filter state service to enable or disable the behavior.
--- ros/behaviors/talk/scripts/talk_node.py
+++ ros/behaviors/talk/scripts/talk_node.py
@@ -5,7 +5,6 @@ import os
 import threading
 
 from google.cloud import texttospeech
-from librosa.core import audio
 
 import numpy as np
 from scipy import signal
@@ -14,38 +13,38 @@ import librosa
 
 import rospy
 import rospkg
-from std_msgs.msg import String, Float32, Empty
+from std_msgs.msg import Float32
+from talk.msg import Text, Done
 from audio_utils.msg import AudioFrame
 
 import hbba_lite
-
-SUPPORTED_SAMPLE_RATE = 22050
 
 
 class TalkNode:
     def __init__(self):
         self._language = rospy.get_param('~language')
         self._mouth_signal_gain = rospy.get_param('~mouth_signal_gain')
+        self._sampling_frequency = rospy.get_param('~sampling_frequency')
         self._frame_sample_count = rospy.get_param('~frame_sample_count')
         self._rospack = rospkg.RosPack()
         self._pkg_path = self._rospack.get_path('talk')
 
         self._mouth_signal_scale_pub = rospy.Publisher('face/mouth_signal_scale', Float32, queue_size=5)
         self._audio_pub = hbba_lite.OnOffHbbaPublisher('audio_out', AudioFrame, queue_size=5)
-        self._done_talking_pub = rospy.Publisher('talk/done', Empty, queue_size=5)
+        self._done_talking_pub = rospy.Publisher('talk/done', Done, queue_size=5)
 
         self._text_sub_lock = threading.Lock()
-        self._text_sub = rospy.Subscriber('talk/text', String, self._on_text_received_cb)
+        self._text_sub = rospy.Subscriber('talk/text', Text, self._on_text_received_cb)
 
     def _on_text_received_cb(self, msg):
         with self._text_sub_lock:
             if self._audio_pub.is_filtering_all_messages:
                 return
 
-            mp3_audio_content = self._generate_mp3_audio_content(msg.data)
+            mp3_audio_content = self._generate_mp3_audio_content(msg.text)
             file_path = self._write_mp3_audio_content(mp3_audio_content)
             self._play_audio(file_path)
-            self._done_talking_pub.publish(Empty())
+            self._done_talking_pub.publish(Done(id=msg.id))
 
     def _generate_mp3_audio_content(self, text):
         language_code = self._convert_language_to_language_code(self._language)
@@ -76,24 +75,20 @@ class TalkNode:
         return file_path
 
     def _play_audio(self, file_path):
-        frames, sample_rate = self._load_frames(file_path)
+        frames = self._load_frames(file_path)
 
-        if sample_rate != SUPPORTED_SAMPLE_RATE:
-            rospy.logerr('Sample rate is not equal to ' + str(SUPPORTED_SAMPLE_RATE) + '.\nSound will not be played')
-            return
-
-        global_energy_filter_sos, global_energy_filter_zi = self._initialize_global_energy_filter(sample_rate)
-        current_energy_filter_sos, current_energy_filter_zi = self._initialize_current_energy_filter(sample_rate)
-        mouth_signal_filter_sos, mouth_signal_filter_zi = self._initialize_mouth_signal_filter(sample_rate)
+        global_energy_filter_sos, global_energy_filter_zi = self._initialize_global_energy_filter()
+        current_energy_filter_sos, current_energy_filter_zi = self._initialize_current_energy_filter()
+        mouth_signal_filter_sos, mouth_signal_filter_zi = self._initialize_mouth_signal_filter()
 
         mouth_signal_msg = Float32()
         audio_frame = AudioFrame()
         audio_frame.format = 'float'
         audio_frame.channel_count = 1
-        audio_frame.sampling_frequency = SUPPORTED_SAMPLE_RATE
+        audio_frame.sampling_frequency = self._sampling_frequency
         audio_frame.frame_sample_count = self._frame_sample_count
 
-        rate = rospy.Rate(SUPPORTED_SAMPLE_RATE / self._frame_sample_count)
+        rate = rospy.Rate(self._sampling_frequency / self._frame_sample_count)
         for frame in frames:
             if self._audio_pub.is_filtering_all_messages:
                 break
@@ -120,22 +115,22 @@ class TalkNode:
         self._mouth_signal_scale_pub.publish(mouth_signal_msg)
 
     def _load_frames(self, file_path):
-        waveform, sample_rate = librosa.load(file_path)
+        waveform, _ = librosa.load(file_path, sr=self._sampling_frequency, res_type='kaiser_fast')
         frames = np.split(waveform, np.arange(self._frame_sample_count, len(waveform), self._frame_sample_count))
-        return frames, sample_rate
+        return frames
 
-    def _initialize_global_energy_filter(self, sample_rate):
-        global_energy_filter_sos =  signal.butter(1, 2, btype='lowpass', fs=sample_rate, output='sos')
+    def _initialize_global_energy_filter(self):
+        global_energy_filter_sos =  signal.butter(1, 2, btype='lowpass', fs=self._sampling_frequency, output='sos')
         global_energy_filter_zi = np.zeros((global_energy_filter_sos.shape[0], 2))
         return global_energy_filter_sos, global_energy_filter_zi
 
-    def _initialize_current_energy_filter(self, sample_rate):
-        current_energy_filter_sos =  signal.butter(1, 10, btype='lowpass', fs=sample_rate, output='sos')
+    def _initialize_current_energy_filter(self):
+        current_energy_filter_sos =  signal.butter(1, 10, btype='lowpass', fs=self._sampling_frequency, output='sos')
         current_energy_filter_zi = np.zeros((current_energy_filter_sos.shape[0], 2))
         return current_energy_filter_sos, current_energy_filter_zi
 
-    def _initialize_mouth_signal_filter(self, sample_rate):
-        mouth_signal_filter_sos =  signal.butter(1, 2.5, btype='lowpass', fs=sample_rate // self._frame_sample_count, output='sos')
+    def _initialize_mouth_signal_filter(self):
+        mouth_signal_filter_sos =  signal.butter(1, 2.5, btype='lowpass', fs=self._sampling_frequency // self._frame_sample_count, output='sos')
         mouth_signal_filter_zi = np.zeros((mouth_signal_filter_sos.shape[0], 2))
         return mouth_signal_filter_sos, mouth_signal_filter_zi
 
