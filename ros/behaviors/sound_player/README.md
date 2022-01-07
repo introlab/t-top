36ff7fffd89e7c256fd94a369a764c8eddd749f8
mamaheux <35638081+mamaheux@users.noreply.github.com>
2022-01-07 13:51:55 -0500

    New behavior nodes (#5)
    * Add an id to the talk node messages.
    
    * Add an id to the gesture node messages.
    
    * Add the sound_player package.
    
    * Add the cloud data package
    
    * Add the new nodes in the launch files.
    
    * Update bringup.launch

---
+++ ros/behaviors/sound_player/README.md
@@ -0,0 +1,20 @@
+# sound_player
+This folder contains the node to play sound files
+
+## Nodes
+### `sound_player_node.py`
+This node plays sound files.
+
+#### Parameters
+ - `sampling_frequency` (int): The output sampling frequency.
+ - `frame_sample_count` (double): The number of sample in each audio frame.
+
+#### Subscribed Topics
+- `sound_player/file` ([sound_player/SoundFile](msg/SoundFile.msg)): The sound file to play.
+
+#### Published Topics
+ - `audio_out` ([audio_utils/AudioFrame](https://github.com/introlab/audio_utils/blob/main/msg/AudioFrame.msg)): The sound (mono, float format).
+ - `sound_player/done` ([sound_player/Done](msg/Done.msg)): Indicates that the speech is finished.
+
+#### Services
+ - `audio_out/filter_state` ([hbba_lite/SetOnOffFilterState](../../hbba_lite/srv/SetOnOffFilterState.srv)): The HBBA filter state service to enable or disable the behavior.
---
+++ ros/behaviors/sound_player/scripts/sound_player_node.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import threading
+
+import numpy as np
+
+import librosa
+
+import rospy
+from sound_player.msg import SoundFile, Done
+from audio_utils.msg import AudioFrame
+
+import hbba_lite
+
+
+class SoundPlayerNode:
+    def __init__(self):
+        self._sampling_frequency = rospy.get_param('~sampling_frequency')
+        self._frame_sample_count = rospy.get_param('~frame_sample_count')
+
+        self._audio_pub = hbba_lite.OnOffHbbaPublisher('audio_out', AudioFrame, queue_size=5)
+        self._done_pub = rospy.Publisher('sound_player/done', Done, queue_size=5)
+
+        self._file_sub_lock = threading.Lock()
+        self._file_sub = rospy.Subscriber('sound_player/file', SoundFile, self._on_file_received_cb, queue_size=1)
+
+    def _on_file_received_cb(self, msg):
+        with self._file_sub_lock:
+            if self._audio_pub.is_filtering_all_messages:
+                return
+
+            try:
+                self._play_audio(msg.path)
+                ok = True
+            except Exception:
+                ok = False
+            self._done_pub.publish(Done(id=msg.id, ok=ok))
+
+    def _play_audio(self, path):
+        frames = self._load_frames(path)
+
+        audio_frame = AudioFrame()
+        audio_frame.format = 'float'
+        audio_frame.channel_count = 1
+        audio_frame.sampling_frequency = self._sampling_frequency
+        audio_frame.frame_sample_count = self._frame_sample_count
+
+        rate = rospy.Rate(self._sampling_frequency / self._frame_sample_count)
+        for frame in frames:
+            if self._audio_pub.is_filtering_all_messages:
+                break
+
+            audio_frame.data = frame.tobytes()
+            self._audio_pub.publish(audio_frame)
+
+            rate.sleep()
+
+    def _load_frames(self, file_path):
+        waveform, _ = librosa.load(file_path, sr=self._sampling_frequency, res_type='kaiser_fast')
+        frames = np.split(waveform, np.arange(self._frame_sample_count, len(waveform), self._frame_sample_count))
+        return frames
+
+    def run(self):
+        rospy.spin()
+
+
+def main():
+    rospy.init_node('sound_player_node')
+    sound_player_node = SoundPlayerNode()
+    sound_player_node.run()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
