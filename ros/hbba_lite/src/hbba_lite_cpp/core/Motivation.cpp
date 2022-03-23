90c2c69f3370d6a3b9bb03890ea0d9ca04bdc3b6
philippewarren <philippewarren31@gmail.com>
2022-03-23 10:25:47 -0400

    Extracted hbba_lite

--- ros/hbba_lite/src/hbba_lite_cpp/core/Motivation.cpp
+++
@@ -1,5 +0,0 @@
-#include <hbba_lite/core/Motivation.h>
-
-using namespace std;
-
-Motivation::Motivation(shared_ptr<DesireSet> desireSet) : m_desireSet(move(desireSet)) {}
--- ros/hbba_lite/src/hbba_lite_cpp/utils/BinarySemaphore.cpp
+++
@@ -1,3 +0,0 @@
-#include <hbba_lite/utils/BinarySemaphore.h>
-
-BinarySemaphore::BinarySemaphore(bool state) : m_state(state) {}
