89d11b9c91499146fa7ac0cfb718a78966e71d84
Marc-Antoine Maheux <Marc-Antoine.Maheux@USherbrooke.ca>
2022-07-18 06:46:01 -0400

    Refactoring

--- tools/dnn_training/common/criterions/jensen_shannon_divergence.py
+++ tools/dnn_training/common/criterions/jensen_shannon_divergence.py
@@ -8,9 +8,9 @@ class JensenShannonDivergence(nn.Module):
         probabilities_1 = F.softmax(logits_1, dim=1)
         probabilities_2 = F.softmax(logits_2, dim=1)
 
-        target_m = 0.5 * (probabilities_1 + probabilities_2)
+        target_m = (probabilities_1 + probabilities_2) / 2.0
         log_target_m = target_m.log()
         loss = F.kl_div(log_target_m, F.log_softmax(logits_1, dim=1), reduction='batchmean', log_target=True) + \
                F.kl_div(log_target_m, F.log_softmax(logits_2, dim=1), reduction='batchmean', log_target=True)
 
-        return 0.5 * loss
+        return loss / 2.0
