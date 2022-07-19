9ba07245cb6dc1629a305199bcded8d616bb8d4c
Marc-Antoine Maheux <Marc-Antoine.Maheux@USherbrooke.ca>
2022-07-19 15:15:14 -0400

    Fix open_face_inception_with_temporal_attention

--- tools/dnn_training/audio_descriptor/backbones/open_face_inception_with_temporal_attention.py
+++ tools/dnn_training/audio_descriptor/backbones/open_face_inception_with_temporal_attention.py
@@ -5,22 +5,33 @@ from common.modules import InceptionModule, PaddedLPPool2d, Lrn2d
 
 
 class TemporalAttention(nn.Module):
-    def __init__(self, n_features):
+    def __init__(self, n_features, in_channels):
         super(TemporalAttention, self).__init__()
+
+        self._feature_layers = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=in_channels // 2, kernel_size=1, bias=False),
+            nn.BatchNorm2d(num_features=in_channels // 2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=in_channels // 2, out_channels=in_channels // 4, kernel_size=3, padding=1,
+                      groups=in_channels // 4, bias=False),
+            nn.BatchNorm2d(in_channels // 4),
+            nn.ReLU(inplace=True)
+        )
 
         self._attention_layers = nn.Sequential(
             nn.Conv2d(in_channels=2, out_channels=2, kernel_size=(n_features, 3), padding=(0, 1), bias=False),
             nn.BatchNorm2d(num_features=2),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=2, out_channels=1, kernel_size=(1, 3), padding=(0, 1)),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=2, out_channels=1, kernel_size=(1, 3), padding=(0, 1), bias=False),
             nn.BatchNorm2d(1),
             nn.Sigmoid()
         )
 
     def forward(self, x):
-        features = torch.cat([torch.mean(x, dim=1, keepdim=True),
-                              torch.max(x, dim=1, keepdim=True)[0]], dim=1)
-        attention = self._attention_layers(features)
+        features = self._feature_layers(x)
+        aggregated_features = torch.cat([torch.mean(features, dim=1, keepdim=True),
+                                         torch.max(features, dim=1, keepdim=True)[0]], dim=1)
+        attention = self._attention_layers(aggregated_features)
         return x * attention
 
 
@@ -33,7 +44,7 @@ class OpenFaceInceptionWithTemporalAttention(nn.Module):
             nn.Conv2d(in_channels=1, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True),
-            TemporalAttention(n_features // 2),
+            TemporalAttention(n_features // 2, in_channels=64),
 
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
             Lrn2d(size=5, alpha=0.0001, beta=0.75),
@@ -44,7 +55,7 @@ class OpenFaceInceptionWithTemporalAttention(nn.Module):
             nn.Conv2d(in_channels=64, out_channels=192, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(num_features=192),
             nn.ReLU(inplace=True),
-            TemporalAttention(n_features // 4),
+            TemporalAttention(n_features // 4, in_channels=192),
 
             Lrn2d(size=5, alpha=0.0001, beta=0.75),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
@@ -67,7 +78,6 @@ class OpenFaceInceptionWithTemporalAttention(nn.Module):
                             out_channels=[256, 64],
                             reduce_size=[128, 32, None, None],
                             pool=nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
-            TemporalAttention(n_features // 16),
 
             InceptionModule(in_channels=640,
                             kernel_size=[3, 5],
@@ -81,7 +91,6 @@ class OpenFaceInceptionWithTemporalAttention(nn.Module):
                             out_channels=[256, 128],
                             reduce_size=[160, 64, None, None],
                             pool=nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
-            TemporalAttention(n_features // 32),
 
             InceptionModule(in_channels=1024,
                             kernel_size=[3],
