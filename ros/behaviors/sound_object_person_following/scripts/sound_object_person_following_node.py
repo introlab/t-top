38c38e285833f2386b4cf8965357c6de341a8905
mamaheux <35638081+mamaheux@users.noreply.github.com>
2023-01-22 16:00:28 -0500

    Improve sound object person following (#72)
    * Add the option to use the bounding boxes or the semantic segmentation in sound_object_person_following_node.py.
    
    * Cleanup

--- ros/behaviors/sound_object_person_following/scripts/sound_object_person_following_node.py
+++ ros/behaviors/sound_object_person_following/scripts/sound_object_person_following_node.py
@@ -99,15 +99,12 @@ class ObjectPersonFollower(Follower):
     def _video_analysis_cb(self, msg):
         if self._movement_commands.is_filtering_all_messages:
             return
-        if len(msg.semantic_segmentation) == 0:
-            rospy.logerr('The video analysis must have semantic segmentation.')
-            return
 
         person = self._find_biggest_person(msg.objects)
         if person is None:
             target = None
         else:
-            target = self._find_target(msg.semantic_segmentation[0], person)
+            target = self._find_target(msg, person)
 
         with self._target_lock:
             self._target = target
@@ -119,31 +116,9 @@ class ObjectPersonFollower(Follower):
 
         return max(person_area_pairs, key= lambda x: x[1])[0]
 
-    def _find_target(self, semantic_segmentation, person, eps=1e-6):
-        min_x, max_x, min_y, max_y = self._find_target_range(person)
-        mask = self._get_mask_from_semantic_segmentation(semantic_segmentation)
-
-        window_width = self._camera_3d_calibration.half_width - self._padding
-        window_height = self._camera_3d_calibration.half_height - self._padding
-
-        def cost(variables):
-            target_x, target_y = variables
-
-            person_center_loss = (person.center_2d.x - target_x)**2 + (person.center_2d.y - target_y)**2
-
-            h, w = semantic_segmentation.shape
-            x0 = np.clip(math.floor((target_x - window_width) * w), 0, w)
-            y0 = np.clip(math.floor((target_y - window_height) * h), 0, h)
-            x1 = np.clip(math.floor((target_x + window_width) * w), 0, w)
-            y1 = np.clip(math.floor((target_y + window_height) * h), 0, h)
-            intersection = mask[y0:y1, x0:x1].sum()
-
-            return self._target_lambda * person_center_loss - intersection
-
-        bounds = [(min_x, max_x + eps), (min_y, max_y + eps)]
-        result = dual_annealing(cost, bounds, maxiter=50)
-
-        return Target.from_object_person_following(result.x[0], result.x[1])
+    @abstractmethod
+    def _find_target(self, msg, person, eps=1e-6):
+        pass
 
     def _find_target_range(self, person):
         half_person_width = person.width_2d / 2
@@ -164,6 +139,99 @@ class ObjectPersonFollower(Follower):
 
         return min_x, max_x, min_y, max_y
 
+    @property
+    def target(self):
+         with self._target_lock:
+            return self._target
+
+
+def _verify_padding(camera_3d_calibration, padding):
+        if camera_3d_calibration.half_width < padding or camera_3d_calibration.half_height < padding:
+            raise ValueError('The padding is too big.')
+
+
+class BoundingBoxObjectPersonFollower(ObjectPersonFollower):
+    def _find_target(self, msg, person, eps=1e-6):
+        min_x, max_x, min_y, max_y = self._find_target_range(person)
+        bounding_boxes = self._get_object_bounding_boxes(self._filter_objects(msg.objects))
+
+        def cost(variables):
+            target_x, target_y = variables
+
+            person_center_loss = (person.center_2d.x - target_x)**2 + (person.center_2d.y - target_y)**2
+
+            if len(bounding_boxes) > 0:
+                wx0 = target_x - self._camera_3d_calibration.half_width
+                wy0 = target_y - self._camera_3d_calibration.half_height
+                wx1 = target_x + self._camera_3d_calibration.half_width
+                wy1 = target_y + self._camera_3d_calibration.half_height
+
+                intersection_w = np.minimum(bounding_boxes[:, 2], wx1) - np.maximum(bounding_boxes[:, 0], wx0)
+                intersection_h = np.minimum(bounding_boxes[:, 3], wy1) - np.maximum(bounding_boxes[:, 1], wy0)
+                intersection_w = np.clip(intersection_w, a_min=0., a_max=None)
+                intersection_h = np.clip(intersection_h, a_min=0., a_max=None)
+                intersection = (intersection_w * intersection_h).sum()
+            else:
+                intersection = 0
+
+            return self._target_lambda * person_center_loss - intersection
+
+        bounds = [(min_x, max_x + eps), (min_y, max_y + eps)]
+        result = dual_annealing(cost, bounds, maxiter=50)
+
+        return Target.from_object_person_following(result.x[0], result.x[1])
+
+    def _filter_objects(self, objects):
+        if self._object_classes == set(['all']):
+            return objects
+        else:
+            return list(filter(lambda o: o.object_class in self._object_classes, objects))
+
+    def _get_object_bounding_boxes(self, objects):
+        bounding_boxes = []
+        for o in objects:
+            half_width = o.width_2d / 2 + self._padding
+            half_height = o.height_2d / 2 + self._padding
+
+            top_lelf_x = o.center_2d.x - half_width
+            top_lelf_y = o.center_2d.y - half_height
+            bottom_right_x = o.center_2d.x + half_width
+            bottom_right_y = o.center_2d.y + half_height
+
+            bounding_boxes.append([top_lelf_x, top_lelf_y, bottom_right_x, bottom_right_y])
+        return np.array(bounding_boxes)
+
+
+class SemanticSegmentationObjectPersonFollower(ObjectPersonFollower):
+    def _find_target(self, msg, person, eps=1e-6):
+        if len(msg.semantic_segmentation) == 0:
+            rospy.logerr('The video analysis must have semantic segmentation.')
+            return
+
+        min_x, max_x, min_y, max_y = self._find_target_range(person)
+        mask = self._get_mask_from_semantic_segmentation(msg.semantic_segmentation[0])
+
+        window_width = self._camera_3d_calibration.half_width - self._padding
+        window_height = self._camera_3d_calibration.half_height - self._padding
+
+        def cost(variables):
+            target_x, target_y = variables
+
+            person_center_loss = (person.center_2d.x - target_x)**2 + (person.center_2d.y - target_y)**2
+
+            h, w = msg.semantic_segmentation[0].shape
+            x0 = np.clip(math.floor((target_x - window_width) * w), 0, w)
+            y0 = np.clip(math.floor((target_y - window_height) * h), 0, h)
+            x1 = np.clip(math.floor((target_x + window_width) * w), 0, w)
+            y1 = np.clip(math.floor((target_y + window_height) * h), 0, h)
+            intersection = mask[y0:y1, x0:x1].sum()
+
+            return self._target_lambda * person_center_loss - intersection
+
+        bounds = [(min_x, max_x + eps), (min_y, max_y + eps)]
+        result = dual_annealing(cost, bounds, maxiter=50)
+
+        return Target.from_object_person_following(result.x[0], result.x[1])
 
     def _get_mask_from_semantic_segmentation(self, semantic_segmentation):
         class_index_by_name = {i: name for i, name in enumerate(semantic_segmentation.class_names)}
@@ -177,16 +245,6 @@ class ObjectPersonFollower(Follower):
             desired_class_indexes = {class_index_by_name[name] for name in self._object_classes}
             return np.isin(class_indexes, desired_class_indexes)
 
-    @property
-    def target(self):
-         with self._target_lock:
-            return self._target
-
-
-def _verify_padding(camera_3d_calibration, padding):
-        if camera_3d_calibration.half_width < padding or camera_3d_calibration.half_height < padding:
-            raise ValueError('The padding is too big.')
-
 
 class SoundObjectPersonFollowingNode:
     def __init__(self):
@@ -197,11 +255,17 @@ class SoundObjectPersonFollowingNode:
         self._head_control_p_gain = rospy.get_param('~head_control_p_gain')
         self._min_head_pitch = rospy.get_param('~min_head_pitch_rad')
         self._max_head_pitch = rospy.get_param('~max_head_pitch_rad')
+        self._object_person_follower_type = rospy.get_param('~object_person_follower_type')
         self._camera_3d_calibration = Camera3dCalibration.load_json_file()
 
         self._movement_commands = MovementCommands(self._simulation)
         self._sound_follower = SoundFollower(self._movement_commands)
-        self._object_person_follower = ObjectPersonFollower(self._camera_3d_calibration, self._movement_commands)
+        if self._object_person_follower_type == 'bounding_box':
+            self._object_person_follower = BoundingBoxObjectPersonFollower(self._camera_3d_calibration, self._movement_commands)
+        elif self._object_person_follower_type == 'semantic_segmentation':
+            self._object_person_follower = SemanticSegmentationObjectPersonFollower(self._camera_3d_calibration, self._movement_commands)
+        else:
+            raise ValueError(f'Invalid object_person_follower_type (object_person_follower_type={object_person_follower_type})')
 
     def run(self):
         while not rospy.is_shutdown():
