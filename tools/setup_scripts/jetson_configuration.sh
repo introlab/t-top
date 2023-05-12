8b259a32a0fe44a86b9243c3fab7a251ce3804e2
philippewarren <philippewarren31@gmail.com>
2023-05-12 11:03:34 -0400

    WIP install script

--- tools/setup_scripts/jetson_configuration.sh
+++ tools/setup_scripts/jetson_configuration.sh
@@ -131,7 +131,7 @@ ECHO_IN_BLUE "###############################################################"
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
 sudo apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
 sudo apt update
-sudo apt install cmake
+sudo apt install -y cmake
 ECHO_IN_BLUE "###############################################################\n"
 
 ECHO_IN_BLUE "###############################################################"
