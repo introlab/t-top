b3049fae89c1d7bdc1c5744b17f3e799d8f98238
Marc-Antoine Maheux <Marc-Antoine.Maheux@USherbrooke.ca>
2023-09-07 15:35:52 -0400

    Fix dnn_units tests.

--- ros/utils/dnn_utils/scripts/test.py
+++ ros/utils/dnn_utils/scripts/test.py
@@ -8,7 +8,7 @@ import torch
 
 import rospy
 
-from dnn_utils import DescriptorYoloV4, YoloV4, PoseEstimator, FaceDescriptorExtractor
+from dnn_utils import DescriptorYoloV4, Yolo, PoseEstimator, FaceDescriptorExtractor
 from dnn_utils import MulticlassAudioDescriptorExtractor, VoiceDescriptorExtractor, TTopKeywordSpotter
 from dnn_utils import SemanticSegmentationNetwork
 
@@ -52,12 +52,12 @@ def test_descriptor_yolo_v4():
             mean_abs_diff(cpu_predictions[i], trt_gpu_predictions[i]))
 
 
-def test_yolo_v4():
-    print('----------test_yolo_v4----------')
+def test_yolo(model_name):
+    print(f'----------test_{model_name}----------')
 
-    cpu_model = YoloV4(inference_type='cpu')
-    torch_gpu_model = YoloV4(inference_type='torch_gpu')
-    trt_gpu_model = YoloV4(inference_type='trt_gpu')
+    cpu_model = Yolo(model_name, inference_type='cpu')
+    torch_gpu_model = Yolo(model_name, inference_type='torch_gpu')
+    trt_gpu_model = Yolo(model_name, inference_type='trt_gpu')
 
     IMAGE_SIZE = cpu_model.get_supported_image_size()
     x = torch.rand(3, IMAGE_SIZE[0], IMAGE_SIZE[1])
@@ -97,12 +97,12 @@ def test_pose_estimator():
           mean_abs_diff(cpu_presence, trt_gpu_presence))
 
 
-def test_face_descriptor_extractor():
-    print('----------test_face_descriptor_extractor----------')
+def test_face_descriptor_extractor(model_name):
+    print(f'----------test_face_descriptor_extractor_{model_name}----------')
 
-    cpu_model = FaceDescriptorExtractor(inference_type='cpu')
-    torch_gpu_model = FaceDescriptorExtractor(inference_type='torch_gpu')
-    trt_gpu_model = FaceDescriptorExtractor(inference_type='trt_gpu')
+    cpu_model = FaceDescriptorExtractor(model_name, inference_type='cpu')
+    torch_gpu_model = FaceDescriptorExtractor(model_name, inference_type='torch_gpu')
+    trt_gpu_model = FaceDescriptorExtractor(model_name, inference_type='trt_gpu')
 
     IMAGE_SIZE = cpu_model.get_supported_image_size()
     x = torch.rand(3, IMAGE_SIZE[0], IMAGE_SIZE[1])
@@ -204,9 +204,13 @@ def main():
     rospy.init_node('dnn_utils_test', disable_signals=True)
 
     launch_test(test_descriptor_yolo_v4)
-    launch_test(test_yolo_v4)
+    launch_test(test_yolo, 'yolo_v4')
+    launch_test(test_yolo, 'yolo_v4_tiny')
+    launch_test(test_yolo, 'yolo_v7')
+    launch_test(test_yolo, 'yolo_v7_tiny')
     launch_test(test_pose_estimator)
-    launch_test(test_face_descriptor_extractor)
+    launch_test(test_face_descriptor_extractor, 'open_face')
+    launch_test(test_face_descriptor_extractor, 'efficientnet_b0')
     launch_test(test_multiclass_audio_descriptor_extractor)
     launch_test(test_voice_descriptor_extractor)
     launch_test(test_ttop_keyword_spotter)
