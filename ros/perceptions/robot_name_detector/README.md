ac1187b80cd7c831ca501c59b1d93cda3d83c913
Marc-Antoine Maheux <Marc-Antoine.Maheux@USherbrooke.ca>
2024-05-28 18:44:55 -0400

    Add robot_name_detector.

---
+++ ros/perceptions/robot_name_detector/README.md
@@ -0,0 +1,31 @@
+# robot_name_detector
+This folder contains the node to detect the robot name.
+
+## Nodes
+### `robot_name_detector_node.py`
+This node measures the sound level and detects the robot name.
+
+#### Parameters
+ - `led_status_duration_s` (double): The LED status duration en seconds. The default value is 1.0.
+ - `sound_presence_relative_threshold` (double): The relative threshold of the sound precense between the slow and fast filter. The default value is 1.05.
+ - `robot_name_model_probability_threshold` (double): The joint probability threshold. The default value is 0.05.
+ - `robot_name_model_interval` (int): The interval between inferences in sample. The default value is 800.
+ - `robot_name_model_analysis_delay` (int): The delay after the rising edge in sample. The default value is 10400.
+ - `robot_name_model_analysis_count` (int): The number of inference to perform. The default value is 3.
+ - `fast_sound_rms_attack` (double): The fast attack value of the sound level filter. The default value is 0.05.
+ - `fast_sound_rms_release` (double): The fast release value of the sound level filter The default value is 0.9.
+ - `slow_sound_rms_attack` (double): The slow attack value of the sound level filter. The default value is 0.975.
+ - `slow_sound_rms_release` (double): The slow release value of the sound level filter The default value is 0.975.
+ - `inference_type` (string): Indicates where to run the neural network (cpu, torch_gpu or trt_gpu). The default value is cpu.
+
+#### Subscribed Topics
+ - `audio_in` ([audio_utils/AudioFrame](https://github.com/introlab/audio_utils/blob/main/msg/AudioFrame.msg)): The sound topic processed, which must be signed_16, at 16000 Hz and 1 channel.
+
+#### Published Topics
+ - `sound_rms` ([std_msgs/Float32](https://docs.ros2.org/foxy/api/std_msgs/msg/Float32.html)): The sound level.
+ - `sound_presence` ([std_msgs/Bool](https://docs.ros2.org/foxy/api/std_msgs/msg/Bool.html)): Indicates if sound is presence.
+ - `robot_name_detected` ([std_msgs/Empty](https://docs.ros2.org/foxy/api/std_msgs/msg/Empty.html)): Indicates that the robot name is detected.
+ - `robot_name_detector/set_led_colors` ([daemon_ros_client/LedColors](../../daemon_ros_client/msg/LedColors.msg)): The LED colors for the status.
+
+#### Services
+ - `audio_in/filter_state` ([hbba_lite/SetOnOffFilterState](../../hbba_lite/srv/SetOnOffFilterState.srv)): The HBBA filter state service to enable or disable the processing.
---
+++ ros/perceptions/robot_name_detector/scripts/robot_name_detector_node.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+import torch
+
+import rclpy
+import rclpy.node
+
+from std_msgs.msg import Float32, Bool, Empty
+from audio_utils.msg import AudioFrame
+from daemon_ros_client.msg import LedColors
+
+from dnn_utils import TTopKeywordSpotter
+import hbba_lite
+
+
+SUPPORTED_AUDIO_FORMAT = 'signed_16'
+SUPPORTED_CHANNEL_COUNT = 1
+
+NONE_LED_COLORS = LedColors()
+for led_color in NONE_LED_COLORS.colors:
+    led_color.red = 0
+    led_color.green = 0
+    led_color.blue = 0
+
+
+class SoundRmsFilter:
+    def __init__(self, attack, release, initial_sound_rms=0.0):
+        self._attack = attack
+        self._release = release
+        self._sound_rms = initial_sound_rms
+
+    def update(self, frame_rms):
+        if frame_rms > self._sound_rms:
+            self._sound_rms = self._attack * self._sound_rms + (1 - self._attack) * frame_rms
+        else:
+            self._sound_rms = self._release * self._sound_rms + (1 - self._release) * frame_rms
+
+        return self._sound_rms
+
+    def reset(self):
+        self._sound_rms = 0.0
+
+
+class RobotNameDetectorNode(rclpy.node.Node):
+    def __init__(self):
+        super().__init__('robot_name_detector_node')
+
+        self._led_status_duration_s = self.declare_parameter('led_status_duration_s', 1.0).get_parameter_value().double_value
+
+        self._sound_presence_relative_threshold = self.declare_parameter('sound_presence_relative_threshold', 1.05).get_parameter_value().double_value
+
+        self._robot_name_model_probability_threshold = self.declare_parameter('robot_name_model_probability_threshold', 0.05).get_parameter_value().double_value
+        self._robot_name_model_interval = self.declare_parameter('robot_name_model_interval', 800).get_parameter_value().integer_value
+        self._robot_name_model_analysis_delay = self.declare_parameter('robot_name_model_analysis_delay', 10400).get_parameter_value().integer_value
+        self._robot_name_model_analysis_count = self.declare_parameter('robot_name_model_analysis_count', 3).get_parameter_value().integer_value
+
+        self._fast_sound_rms_filter = SoundRmsFilter(self.declare_parameter('fast_sound_rms_attack', 0.05).get_parameter_value().double_value,
+                                                     self.declare_parameter('fast_sound_rms_release', 0.99).get_parameter_value().double_value)
+        self._slow_sound_rms_filter = SoundRmsFilter(self.declare_parameter('slow_sound_rms_attack', 0.975).get_parameter_value().double_value,
+                                                     self.declare_parameter('slow_sound_rms_release', 0.975).get_parameter_value().double_value,
+                                                     initial_sound_rms=0.1)
+
+        self._inference_type = self.declare_parameter('inference_type', 'cpu').get_parameter_value().string_value
+        self._robot_name_model = TTopKeywordSpotter(inference_type=self._inference_type)
+        self._robot_name_model_output_index = self._robot_name_model.get_class_names().index('T-Top')
+
+        self._robot_name_model_buffer = torch.zeros(self._robot_name_model.get_supported_duration(), dtype=torch.float32)
+        self._robot_name_model_interval_count = self._robot_name_model_interval - self._robot_name_model_analysis_delay
+        self._robot_name_model_analysis_enabled = False
+        self._robot_name_model_analysis_waiting = False
+        self._robot_name_model_probabilities = []
+
+        self._detection_status = None
+        self._detection_time_s = 0.0
+
+        # Warm up, to avoid inference delays
+        self._robot_name_model(self._robot_name_model_buffer)
+
+        self._fast_sound_rms_pub = self.create_publisher(Float32, 'fast_sound_rms', 10)
+        self._slow_sound_rms_pub = self.create_publisher(Float32, 'slow_sound_rms', 10)
+        self._sound_presence_pub = self.create_publisher(Bool, 'sound_presence', 10)
+        self._robot_name_detected_pub = self.create_publisher(Empty, 'robot_name_detected', 10)
+
+        self._led_colors_pub = hbba_lite.OnOffHbbaPublisher(self, LedColors, 'robot_name_detector/set_led_colors', 1,
+                                                            state_service_name='led_status/filter_state')
+        self._led_colors_pub.on_filter_state_changing(self._led_colors_hbba_filter_state_cb)
+
+        self._hbba_filter_state = hbba_lite.OnOffHbbaFilterState(self, 'audio_in/filter_state')
+        self._hbba_filter_state.on_changed(self._robot_name_detector_hbba_state_changed_cb)
+        self._audio_sub = self.create_subscription(AudioFrame, 'audio_in', self._audio_cb, 100)
+
+    def _led_colors_hbba_filter_state_cb(self, publish_forced,
+                                         previous_is_filtering_all_messages, new_is_filtering_all_messages):
+        if not previous_is_filtering_all_messages and new_is_filtering_all_messages:
+            publish_forced(NONE_LED_COLORS)
+
+    def _robot_name_detector_hbba_state_changed_cb(self, previous_is_filtering_all_messages, new_is_filtering_all_messages):
+        self._fast_sound_rms_filter.reset()
+
+    def _audio_cb(self, msg):
+        if msg.format != SUPPORTED_AUDIO_FORMAT or \
+                msg.channel_count != SUPPORTED_CHANNEL_COUNT or \
+                msg.sampling_frequency != self._robot_name_model.get_supported_sampling_frequency():
+            self.get_logger().error(
+                f'Invalid audio frame (msg.format={msg.format}, msg.channel_count={msg.channel_count}, msg.sampling_frequency={msg.sampling_frequency})')
+            return
+
+        audio_frame = np.frombuffer(msg.data, dtype=np.int16).astype(np.float32) / -np.iinfo(np.int16).min
+        frame_rms = np.sqrt(np.mean(audio_frame**2))
+        fast_sound_rms = self._fast_sound_rms_filter.update(frame_rms)
+        slow_sound_rms = self._slow_sound_rms_filter.update(frame_rms)
+        presence = fast_sound_rms > (slow_sound_rms * self._sound_presence_relative_threshold)
+
+        self._publish_sound_rms_messages(fast_sound_rms, slow_sound_rms, presence)
+
+        if not self._hbba_filter_state.is_filtering_all_messages:
+            self._detect_robot_name(audio_frame, presence)
+        if not self._led_colors_pub.is_filtering_all_messages:
+            self._publish_led_status(fast_sound_rms, slow_sound_rms)
+
+    def _publish_sound_rms_messages(self, fast_sound_rms, slow_sound_rms, presence):
+        sound_rms_msg = Float32()
+
+        sound_rms_msg.data = fast_sound_rms
+        self._fast_sound_rms_pub.publish(sound_rms_msg)
+
+        sound_rms_msg.data = slow_sound_rms
+        self._slow_sound_rms_pub.publish(sound_rms_msg)
+
+        sound_presence_msg = Bool()
+        sound_presence_msg.data = bool(presence)
+        self._sound_presence_pub.publish(sound_presence_msg)
+
+    def _detect_robot_name(self, audio_frame, presence):
+        self._update_robot_name_buffer(audio_frame)
+
+        if presence and not self._robot_name_model_analysis_enabled:
+            self._robot_name_model_analysis_enabled = True
+            self._robot_name_model_analysis_waiting = False
+
+        if len(self._robot_name_model_probabilities) >= self._robot_name_model_analysis_count:
+            self._publish_robot_name_detected_if_needed()
+            self._robot_name_model_interval_count = self._robot_name_model_interval - self._robot_name_model_analysis_delay
+            self._robot_name_model_analysis_waiting = True
+            self._robot_name_model_probabilities = []
+        elif self._robot_name_model_analysis_enabled and not self._robot_name_model_analysis_waiting:
+            self._robot_name_model_interval_count += audio_frame.shape[0]
+            self._detect_robot_name_if_needed()
+        elif self._robot_name_model_analysis_waiting and not presence:
+            self._robot_name_model_analysis_waiting = False
+            self._robot_name_model_analysis_enabled = False
+
+    def _update_robot_name_buffer(self, audio_frame):
+        self._robot_name_model_buffer = torch.roll(self._robot_name_model_buffer, -audio_frame.shape[0])
+        self._robot_name_model_buffer[-audio_frame.shape[0]:] = torch.from_numpy(audio_frame.copy())
+
+    def _publish_robot_name_detected_if_needed(self):
+        joint_probability = np.prod(np.array(self._robot_name_model_probabilities))
+
+        if joint_probability > self._robot_name_model_probability_threshold:
+            self._robot_name_detected_pub.publish(Empty())
+            self._detection_status = True
+        else:
+            self._detection_status = False
+
+        self._detection_time_s = self._now_s()
+
+    def _detect_robot_name_if_needed(self):
+        if self._robot_name_model_interval_count >= self._robot_name_model_interval:
+            probabilities = self._robot_name_model(self._robot_name_model_buffer)
+
+            self._robot_name_model_interval_count = 0
+            self._robot_name_model_probabilities.append(probabilities[self._robot_name_model_output_index].item())
+
+    def _publish_led_status(self, fast_sound_rms, slow_sound_rms, eps=1e-6):
+        if self._now_s() - self._detection_time_s < self._led_status_duration_s and self._detection_status is not None:
+            if self._detection_status:
+                self._publish_led_colors(0, 255, 0)
+            else:
+                self._publish_led_colors(255, 0, 0)
+        else:
+            one_level = slow_sound_rms * self._sound_presence_relative_threshold
+            zero_level = slow_sound_rms
+            one_zero_diff = one_level - zero_level
+            level = np.clip((fast_sound_rms - zero_level) / (one_zero_diff + eps), a_min=0.0, a_max=1.0)
+
+            self._publish_led_colors(int(255 * level), int(255 * level), int(255 * level))
+
+    def _publish_led_colors(self, red, green, blue):
+        msg = LedColors()
+        for led_color in msg.colors:
+            led_color.red = red
+            led_color.green = green
+            led_color.blue = blue
+
+        self._led_colors_pub.publish(msg)
+
+    def run(self):
+        rclpy.spin(self)
+
+    def _now_s(self):
+        return self.get_clock().now().nanoseconds / 1e9
+
+
+def main():
+    rclpy.init()
+
+    robot_name_detector_node = RobotNameDetectorNode()
+    robot_name_detector_node.run()
+
+    robot_name_detector_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
