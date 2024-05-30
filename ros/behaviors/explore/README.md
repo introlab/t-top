d76d2d125a9ff644d327a4f3d8b9cb6fd8d5a8dc
Marc-Antoine Maheux <Marc-Antoine.Maheux@USherbrooke.ca>
2024-05-30 09:39:38 -0400

    Add explore.

---
+++ ros/behaviors/explore/README.md
@@ -0,0 +1,34 @@
+# explore
+
+This folder contains the node to make T-Top explore its surrounding.
+
+## Nodes
+
+### `explore_node.py`
+
+This node makes T-Top explore its surrounding.
+
+#### Parameters
+
+- `simulation` (bool): Indicates if it's used in the simulation. The default value is false.
+- `explore_frequency` (double): The frequency at which T-Top explores its surrounding. The default value is 0.00833333333.
+- `torso_speed_rad_sec` (double): The torso speed in rad/s. The default value is 0.5.
+- `head_speed_rad_sec` (double): The head speed in rad/s. The default value is 0.5.
+
+#### Subscribed Topics
+
+- `daemon/motor_status` ([daemon_ros_client/MotorStatus](../../daemon_ros_client/msg/MotorStatus.msg)): The motor status.
+
+#### Published Topics
+
+- `explore/set_head_pose` ([geometry_msgs/PoseStamped](http://docs.ros.org/en/noetic/api/geometry_msgs/html/msg/PoseStamped.html)):
+  The head pose.
+- `explore/set_torso_orientation` ([std_msgs/Float32](http://docs.ros.org/en/noetic/api/std_msgs/html/msg/Float32.html)): The
+  torso orientation.
+- `explore/done` ([std_msgs/Empty](http://docs.ros.org/en/noetic/api/std_msgs/html/msg/Empty.html)): Indicates that the
+  explore moves are finished.
+
+#### Services
+
+- `pose/filter_state` ([hbba_lite/SetOnOffFilterState](../../hbba_lite/srv/SetOnOffFilterState.srv)): The HBBA filter
+  state service to enable or disable the behavior.
---
+++ ros/behaviors/explore/scripts/explore_node.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import rclpy
+import rclpy.node
+
+from std_msgs.msg import Empty
+
+from t_top import MovementCommands, HEAD_ZERO_Z
+
+
+INACTIVE_SLEEP_DURATION = 0.1
+
+
+class ExploreNode(rclpy.node.Node):
+    def __init__(self):
+        super().__init__('explore_node')
+
+        self._simulation = self.declare_parameter('simulation', False).get_parameter_value().bool_value
+        self._explore_frequency = self.declare_parameter('explore_frequency', 0.00833333333).get_parameter_value().double_value
+        self._torso_speed = self.declare_parameter('torso_speed_rad_sec', 0.5).get_parameter_value().double_value
+        self._head_speed = self.declare_parameter('head_speed_rad_sec', 0.5).get_parameter_value().double_value
+
+        self._timer = self.create_timer(1 / self._explore_frequency, self._timer_callback)
+
+        self._movement_commands = MovementCommands(self, self._simulation, namespace='explore')
+        self._done_pub = self.create_publisher(Empty, 'explore/done', 5)
+
+    def _timer_callback(self):
+        if self._movement_commands.is_filtering_all_messages:
+            return
+
+        self._movement_commands.move_torso(0, should_wait=True, speed_rad_sec=self._torso_speed)
+        self._movement_commands.move_head([0, 0, HEAD_ZERO_Z, 0, -0.3, 0], should_wait=True, speed_rad_sec=self._head_speed)
+        self._movement_commands.move_torso(1.57, should_wait=False, speed_rad_sec=self._torso_speed)
+        self._movement_commands.move_torso(3.14, should_wait=True, speed_rad_sec=self._torso_speed)
+        self._movement_commands.move_head([0, 0, HEAD_ZERO_Z, 0, 0.15, 0], should_wait=True, speed_rad_sec=self._head_speed)
+        self._movement_commands.move_torso(1.57, should_wait=False, speed_rad_sec=self._torso_speed)
+        self._movement_commands.move_torso(0, should_wait=False, speed_rad_sec=self._torso_speed)
+        self._movement_commands.move_torso(-1.57, should_wait=False, speed_rad_sec=self._torso_speed)
+        self._movement_commands.move_torso(-3.14, should_wait=True, speed_rad_sec=self._torso_speed)
+        self._movement_commands.move_head([0, 0, HEAD_ZERO_Z, 0, -0.3, 0], should_wait=True, speed_rad_sec=self._head_speed)
+        self._movement_commands.move_torso(-1.57, should_wait=False, speed_rad_sec=self._torso_speed)
+        self._movement_commands.move_torso(0, should_wait=True, speed_rad_sec=self._torso_speed)
+        self._movement_commands.move_head([0, 0, HEAD_ZERO_Z, 0, 0, 0], should_wait=True, speed_rad_sec=self._head_speed)
+
+        self._done_pub.publish(Empty())
+
+    def run(self):
+        rclpy.spin(self)
+
+
+def main():
+    rclpy.init()
+    explore_node = ExploreNode()
+
+    try:
+        explore_node.run()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        explore_node.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
